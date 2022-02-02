165054c9e02e484ce568d4a10586e567b31122b3
tejalkhandewal <93588531+tejalkhandewal@users.noreply.github.com>
2022-02-02 10:19:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+var firstname="ranbir"
+var lastname="kapoor"
+console.log(firstname)
+console.log(lastname)
+var fullname=firstname.concat(lastname)
+console.log(fullname)
+var a="10"
+var b="5"
+var add= a+b
+console.log(add)
